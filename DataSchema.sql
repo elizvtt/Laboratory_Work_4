e99688becee53e366767c6696eb5e286e13412c2
elizvtt <127402789+elizvtt@users.noreply.github.com>
2024-10-26 11:21:50 +0300

    commit

--- DataSchema.sql
+++ DataSchema.sql
@@ -1,84 +1,49 @@
 -- Опис педагогів
-CREATE TABLE Teachers (
-    teacher_id NUMBER(4), 
-    full_name VARCHAR(50) NOT NULL,
-    position VARCHAR(30) NOT NULL
+CREATE TABLE teachers ( 
+    teacher_id NUMBER(4) PRIMARY KEY,
+    full_name VARCHAR(50) NOT NULL CHECK (
+        REGEXP_LIKE(full_name, '^[A-ZА-Я][a-zа-я]+\s[A-ZА-Я][a-zа-я]+$')
+    ),
+    position VARCHAR(30) NOT NULL CHECK (
+        position IN ('Teacher', 'Assistant', 'Professor')
+    )
 );
-
--- Обмеження первинного ключа
-ALTER TABLE Teachers ADD CONSTRAINT teachers_pk
-    PRIMARY KEY (teacher_id);
 
 -- Опис навчальних матеріалів
-CREATE TABLE Materials (
-    material_id NUMBER(4), 
+CREATE TABLE materials ( 
+    material_id NUMBER(4) PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    contents TEXT NOT NULL, 
-    date_created TIMESTAMP NOT NULL, 
-    teacher_id NUMBER(4) NOT NULL 
+    contents VARCHAR(1000) NOT NULL,
+    date_created TIMESTAMP NOT NULL,
+    teacher_id NUMBER(4) NOT NULL,
+    FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id)
 );
-
--- Обмеження первинного ключа
-ALTER TABLE Materials ADD CONSTRAINT materials_pk
-    PRIMARY KEY (material_id);
-
--- Обмеження зовнішнього ключа
-ALTER TABLE Materials ADD CONSTRAINT materials_teacher_fk
-    FOREIGN KEY (teacher_id) 
-    REFERENCES Teachers (teacher_id);
-
--- Опис рекомендацій
-CREATE TABLE Recommendations (
-    recommendation_id NUMBER(4), 
-    recommendation_description TEXT NOT NULL, 
-    recommendation_date TIMESTAMP NOT NULL, 
-    material_id NUMBER(4) NOT NULL,
-    analysis_id NUMBER(4) NOT NULL 
-);
-
--- Обмеження первинного ключа
-ALTER TABLE Recommendations ADD CONSTRAINT recommendations_pk
-    PRIMARY KEY (recommendation_id);
-
--- Обмеження зовнішнього ключа
-ALTER TABLE Recommendations ADD CONSTRAINT recommendations_material_fk
-    FOREIGN KEY (material_id) 
-    REFERENCES Materials (material_id);
-
-ALTER TABLE Recommendations ADD CONSTRAINT recommendations_analysis_fk
-    FOREIGN KEY (analysis_id) 
-    REFERENCES Analyses (analysis_id);
-
--- Опис аналізу
-CREATE TABLE Analyses (
-    analysis_id NUMBER(4), 
-    result VARCHAR(100) NOT NULL, 
-    date_analysis TIMESTAMP NOT NULL, 
-    material_id NUMBER(4) NOT NULL 
-);
-
--- Обмеження первинного ключа
-ALTER TABLE Analyses ADD CONSTRAINT analyses_pk
-    PRIMARY KEY (analysis_id);
-
--- Обмеження зовнішнього ключа
-ALTER TABLE Analyses ADD CONSTRAINT analyses_material_fk
-    FOREIGN KEY (material_id) 
-    REFERENCES Materials (material_id);
 
 -- Опис вологості
-CREATE TABLE Humidities (
-    humidity_id NUMBER(4), 
-    value NUMBER(5,2) NOT NULL, 
-    date_measurement TIMESTAMP NOT NULL, 
-    teacher_id NUMBER(4) NOT NULL 
+CREATE TABLE humidities ( 
+    humidity_id NUMBER(4) PRIMARY KEY,
+    value NUMBER(5,2) NOT NULL,
+    date_measurement TIMESTAMP NOT NULL,
+    teacher_id NUMBER(4) NOT NULL,
+    FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id)
 );
 
--- Обмеження первинного ключа
-ALTER TABLE Humidities ADD CONSTRAINT humidities_pk
-    PRIMARY KEY (humidity_id);
+-- Опис аналізу
+CREATE TABLE analyses (
+    analysis_id NUMBER(4) PRIMARY KEY,
+    result VARCHAR(100) NOT NULL,
+    date_analysis TIMESTAMP NOT NULL,
+    material_id NUMBER(4) NOT NULL,
+    FOREIGN KEY (material_id) REFERENCES materials (material_id)
+);
 
--- Обмеження зовнішнього ключа
-ALTER TABLE Humidities ADD CONSTRAINT humidities_teacher_fk
-    FOREIGN KEY (teacher_id) 
-    REFERENCES Teachers (teacher_id);
+-- Опис рекомендацій
+CREATE TABLE recommendations (
+    recommendation_id NUMBER(4) PRIMARY KEY,
+    recommendation_description TEXT NOT NULL,
+    recommendation_date TIMESTAMP NOT NULL,
+    material_id NUMBER(4) NOT NULL,
+    analysis_id NUMBER(4) NOT NULL,
+    FOREIGN KEY (material_id) REFERENCES materials (material_id),
+    FOREIGN KEY (analysis_id) REFERENCES analyses (analysis_id)
+);
